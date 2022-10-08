06b23490314f43e1d1f59eb5a7c3bb93c0a1236e
Urmila Kadam <kadamurmila744@gmail.com>
2022-10-08 20:32:53 +0530

    557. Reverse Words in a String III

---
+++ src/ReverseWords557.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ReverseWords557 {
+
+    public static void main(String[] args) {
+        String str = "This is problem statement";
+        System.out.println(reverseWords(str));
+    }
+
+    public static String reverseWords(String s) {
+
+        char[] charArray = s.toCharArray();
+
+        for(int i=0; i<s.length(); i++) {
+
+            int low = i;
+            while(++i < s.length() && charArray[i] != ' ');
+            int high = i - 1;
+
+            while (high > low) {
+                char c = charArray[high];
+                charArray[high] = charArray[low];
+                charArray[low] = c;
+                high--;
+                low++;
+            }
+        }
+        return new String(charArray);
+    }
+
+}
